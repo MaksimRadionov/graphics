15fc9d3e06851c4320ea061caf235b60a852bafb
MaksimRadionov <radionov.work@gmail.com>
2019-02-08 22:47:53 +0300

    Add specular

--- graphics.cpp
+++ graphics.cpp
@@ -16,10 +16,13 @@ struct Light
 
 struct Material 
 {
-    Material(const Vec3f &color) : diffuse_color(color) {}
-    Material() : diffuse_color() {}
+    Material(const Vec2f &a, const Vec3f &color, const float &spec) : albedo(a), diffuse_color(color), specular_exponent(spec) {}
+    Material() : albedo(1,0), diffuse_color(), specular_exponent() {}
+    Vec2f albedo;
     Vec3f diffuse_color;
+    float specular_exponent;
 };
+
 
 struct Sphere 
 {
@@ -46,6 +49,11 @@ struct Sphere
     }
 };
 
+Vec3f reflect(const Vec3f &I, const Vec3f &N) 
+{
+    return I - N*2.f*(I*N);
+}
+
 bool scene_intersect(const Vec3f &orig, const Vec3f &dir, const std::vector<Sphere> &spheres, 
                      Vec3f &hit, Vec3f &N, Material &material) 
 {
@@ -71,13 +79,15 @@ Vec3f cast_ray(const Vec3f &orig, const Vec3f &dir, const std::vector<Sphere> &s
     if (!scene_intersect(orig, dir, spheres, point, N, material))
         return Vec3f(0.2, 0.7, 0.8); // background color
     
-    float diffuse_light_intensity = 0;
+    float diffuse_light_intensity = 0, specular_light_intensity = 0;
     for (size_t i=0; i<lights.size(); i++) 
     {
-        Vec3f light_dir      = (lights[i].position - point).normalize();
+        Vec3f light_dir = (lights[i].position - point).normalize();//L - направление на источник от поверхоолоности
         diffuse_light_intensity  += lights[i].intensity * std::max(0.f, light_dir*N);
+        specular_light_intensity += powf(std::max(0.f, -reflect(-light_dir, N)*dir), material.specular_exponent)*lights[i].intensity;//reflect - отражение dir - направлению в камеру
     }
-    return material.diffuse_color * diffuse_light_intensity;
+    return material.diffuse_color * diffuse_light_intensity * material.albedo[0] + Vec3f(1., 1., 1.)*specular_light_intensity * material.albedo[1];//Vec3f(1., 1., 1.) - типа цвет specular 
+
 }
 
 void render(const std::vector<Sphere> &spheres, const std::vector<Light> &lights,std::string file_name) 
@@ -88,8 +98,10 @@ void render(const std::vector<Sphere> &spheres, const std::vector<Light> &lights
     std::vector<Vec3f> framebuffer(width*height);
 
     #pragma omp parallel for
-    for (size_t j = 0; j<height; j++) {
-        for (size_t i = 0; i<width; i++) {
+    for (size_t j = 0; j<height; j++) 
+    {
+        for (size_t i = 0; i<width; i++) 
+        {
             float x =  (2*(i + 0.5)/(float)width  - 1)*tan(fov/2.)*width/(float)height;
             float y = -(2*(j + 0.5)/(float)height - 1)*tan(fov/2.);//экран помещен на расстояние 1 от начала! типа там фокус что ли. смотри wiki в REadme.
             Vec3f dir = Vec3f(x, y, -1).normalize();
@@ -103,6 +115,10 @@ void render(const std::vector<Sphere> &spheres, const std::vector<Light> &lights
     ofs << "P6\n" << width << " " << height << "\n255\n";
     for (size_t i = 0; i < height*width; ++i) 
     {
+        Vec3f &c = framebuffer[i];                       //так понимаю что это нормировка на единицу
+        float max = std::max(c[0], std::max(c[1], c[2]));//каждого цвета. Если какой то цвет превышает 1(интенсивность) то все 3 
+        if (max>1)                                       //цвета перенормировывают
+            c = c*(1./max);
         for (size_t j = 0; j<3; j++) 
         {
             ofs << (char)(255 * std::max(0.f, std::min(1.f, framebuffer[i][j])));
@@ -115,25 +131,23 @@ void render(const std::vector<Sphere> &spheres, const std::vector<Light> &lights
 }
 int main() 
 {
-    Material      ivory(Vec3f(0., 1., 0.));
-    Material red_rubber(Vec3f(0.3, 0.1, 0.1));
-    
+    Material      ivory(Vec2f(0.6,  0.3), Vec3f(0.4, 0.4, 0.3),   50.);
+    Material red_rubber(Vec2f(0.9,  0.1), Vec3f(0.3, 0.1, 0.1),   10.);
 
     std::vector<Sphere> spheres;
     spheres.push_back(Sphere(Vec3f(-3,    0,   -16), 2,      ivory));
     spheres.push_back(Sphere(Vec3f(-1.0, -1.5, -12), 2, red_rubber));
     spheres.push_back(Sphere(Vec3f( 1.5, -0.5, -18), 3, red_rubber));
     spheres.push_back(Sphere(Vec3f( 7,    5,   -18), 4,      ivory));
-    for(float i =0 ; i<2*M_PI; i+=0.1)
-    {
-        static int j =0;
-        j++;
-        std::cout<<"a "<<20*cos(i)<<" "<<20*sin(i)<<std::endl;
-        std::vector<Light>  lights;
-        lights.push_back(Light(Vec3f(20*cos(i), 20*sin(i),  20), 1.5));
 
-        render(spheres, lights,std::string(std::to_string(j)+".ppm"));
-    }
+    std::vector<Light>  lights;
+    lights.push_back(Light(Vec3f(-20, 20,  20), 1.5));
+    lights.push_back(Light(Vec3f( 30, 50, -25), 1.8));
+    lights.push_back(Light(Vec3f( 30, 20,  30), 1.7));
+
+    render(spheres, lights,"2");
+
     return 0;
+  
 }
 
